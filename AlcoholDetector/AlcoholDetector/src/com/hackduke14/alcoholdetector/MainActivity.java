057dd18be24a1e953553e2cc76bf2fa7d7ebe364
saicheemalapati <saicheemalapati@gmail.com>
2014-03-30 08:25:14 -0400

    removed settings tab

--- AlcoholDetector/AlcoholDetector/src/com/hackduke14/alcoholdetector/MainActivity.java
+++ AlcoholDetector/AlcoholDetector/src/com/hackduke14/alcoholdetector/MainActivity.java
@@ -1,20 +1,13 @@
 package com.hackduke14.alcoholdetector;
 
-import java.io.IOException;
-
 import android.app.Activity;
-import android.app.ActionBar;
 import android.app.Fragment;
-import android.bluetooth.BluetoothAdapter;
-import android.bluetooth.BluetoothDevice;
-import android.bluetooth.BluetoothSocket;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.os.Build;
 
 public class MainActivity extends Activity {
 
@@ -65,7 +58,6 @@ public class MainActivity extends Activity {
             View rootView = inflater.inflate(R.layout.fragment_main, container, false);
             return rootView;
         }
-        
     }
 
 }
